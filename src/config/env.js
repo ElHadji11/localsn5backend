68e0fb894d75ad92300df513c205cac65c88c89c
ElHadji11 <babacar.thior19@gmail.com>
2025-08-17 11:21:52 +0000

    tiny changes

--- src/config/env.js
+++ src/config/env.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV = {
-    PORT: process.env.PORT,
-    NODE_ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT || 5001,
+    NODE_ENV: process.env.NODE_ENV || "development",
     MONGO_URI: process.env.MONGO_URI,
     CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
@@ -13,3 +13,9 @@ export const ENV = {
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
     ARCJET_KEY: process.env.ARCJET_KEY,
 };
+
+//LOGS 
+console.log("Environment variables loaded:");
+console.log("PORT:", ENV.PORT);
+console.log("NODE_ENV:", ENV.NODE_ENV);
+console.log("MONGO_URI exists:", !!ENV.MONGO_URI);
